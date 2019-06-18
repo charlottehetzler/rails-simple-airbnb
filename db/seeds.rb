8c62bd2f81584f1a322f76b88eabb01e9be48a5e
Charlotte Hetzler <charlotte@hetzler.de>
2019-06-18 16:47:27 +0300

    Simple Airbnb - Added views, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+ {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+},
+ {
+  name: 'Awesome Pavillion',
+  address: 'Desert 5a',
+  description: 'classy pavillion in the middle of nowhere',
+  price_per_night: 79,
+  number_of_guests: 3
+},
+ {
+  name: 'Igloo',
+  address: 'Arctic 1',
+  description: 'lovely cool place',
+  price_per_night: 50,
+  number_of_guests: 4
+},
+ {
+  name: 'Tiny Flat',
+  address: 'Hong Kong Island',
+  description: 'Minimal living at its best',
+  price_per_night: 150,
+  number_of_guests: 1
+},
+]
+
+Flat.create!(flats_attributes)
+puts 'Finished!'
